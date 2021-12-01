74e5d55cef8ddc623e41bdfa04ac989a8baf3732
tales dev <tales.ferreira@carteiraglobal.com>
2021-11-30 21:29:50 -0300

    add pow function

--- src/index.js
+++ src/index.js
@@ -12,9 +12,23 @@ function getMiddleLetters(string){
     else return (comprimento + "impar");
 }
 
+function pow(base, expoent){
+    if(expoent < 0) return;
+    if(base === 1) return 1;
+
+    let potencia = 1;
+
+    for ( let i = 0 ; i < expoent; i++){
+        potencia *= base;
+   }
+   return potencia
+}
+
+
 const a = [1, 2, 2, 2, 3, 4, 5];
 
 // console.log(sortArray(a, "dec"));
 // console.log(sortArray(a));
 // console.log(arrayCount(a,2));
-console.log(getMiddleLetters("Tales Ferreira"));
+// console.log(getMiddleLetters("Tales Ferreira"));
+console.log(pow(8,2))
